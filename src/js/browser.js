2a54cd1e073cda347b2c3a9ec08aacacb5fffd24
= <110171601+max-russell1@users.noreply.github.com>
2024-01-18 11:47:51 +0000

    Fix comments

--- src/js/browser.js
+++ src/js/browser.js
@@ -15,7 +15,7 @@ const Browser = {
   isIE: () => {
     return navigator.userAgent.indexOf('MSIE') !== -1 || !!document.documentMode
   },
-  /// Edge 20+
+  // Edge 20+
   isEdge: () => {
     return !Browser.isIE() && !!window.StyleMedia
   },
@@ -23,7 +23,7 @@ const Browser = {
   isChrome: (context = window) => {
     return !!context.chrome
   },
-  /// At least Safari 3+: "[object HTMLElementConstructor]"
+  // At least Safari 3+: "[object HTMLElementConstructor]"
   isSafari: () => {
     return Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0 ||
         navigator.userAgent.toLowerCase().indexOf('safari') !== -1
